0e850fe58b4cbdee52f3de7db037c542a345b8a3
Alexis Alva <alexis.alva@sansano.usm.cl>
2024-07-03 19:41:58 -0400

    Update getHistoricalEventsByOcurrence to add the 400 error responde in case of a number appears in the url

--- src/domain/historical_events/use_cases/getHistoricalEvents.js
+++ src/domain/historical_events/use_cases/getHistoricalEvents.js
@@ -1,6 +1,18 @@
 import historicalEventsRepository from '../repository/historicalEventsRepository'
 
 exports.getHistoricalEventsByOcurrence = (ctx) => {
-    ctx.body = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
+    const ocurrence = ctx.params.ocurrence
+    if (!(containsNumber(ocurrence))){
+        ctx.status = 400;
+        ctx.message = "El input debe ser ac o dc";
+    }
+    else {
+        ctx.body = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
+    }
     return ctx
+}
+
+function containsNumber(str) {
+    // Check if the string contains any digit between 0 and 9
+    return /\d/.test(str);
 }
